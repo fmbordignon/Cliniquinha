e714ea1f3bd9a95c239e1200ac079e76f1ccbfcb
Vinicius Cerutti <vinicius.cerutti@acad.pucrs.br>
2017-08-10 14:30:29 -0300

    Adicionado a Pasta do Projeto

---
+++ ProjetoCliniquinha/Cliniquinha/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Cliniquinha
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
