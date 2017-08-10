5ff60c49ee0a193b3c566a0a575cd71a6987d619
Vinicius Cerutti <vinicius.cerutti@acad.pucrs.br>
2017-08-10 16:50:20 -0300

    Adicionado Projeto Cliniquinha

---
+++ ProjetoCliniquinha/ProjetoCliniquinha/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetoCliniquinha.Startup))]
+namespace ProjetoCliniquinha
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
