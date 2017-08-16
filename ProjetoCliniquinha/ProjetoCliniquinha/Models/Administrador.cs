ff8fa78d2951c2d3ba9e0620a3f9a1246ea1969c
Vinicius Cerutti <vinicius.cerutti@acad.pucrs.br>
2017-08-16 16:50:53 -0300

    (Administrador.cs) Arrumado os prop de acordo com banco

--- ProjetoCliniquinha/ProjetoCliniquinha/Models/Administrador.cs
+++ ProjetoCliniquinha/ProjetoCliniquinha/Models/Administrador.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.WebPages.Html;
 
 namespace ProjetoCliniquinha.Models
 {
@@ -10,9 +11,26 @@ namespace ProjetoCliniquinha.Models
     {
         [Key]
         public int IDAdmin { get; set; }
+        [Required]
+        [Display(Name = "Nome Completo")]
         public string Nome { get; set; }
+
+        [Required]
+        [Display(Name = "RG")]
         public long RG { get; set; }
+        [Display(Name = "Telefone")]
+
+        [Required]
         public long Telefone { get; set; }
+
+        [Required]
+        [Display(Name = "Endereço")]
         public string Endereco { get; set; }
+
+        [Required]
+        [Display(Name = "Especialização")]
+        public string Especializacao { get; set; }
+
+        public IEnumerable<SelectListItem> RolesList { get; set; }
     }
 }
