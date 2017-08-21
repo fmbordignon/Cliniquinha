1fd239baf227f6ef5172faf3cf6d6c8f36b21759
Vinicius Cerutti <vinicius.cerutti@acad.pucrs.br>
2017-08-21 16:36:01 -0300

    Adicionado novo Projeto da clinica

---
+++ ProjetoClinicaV2.0/ProjetoClinicaV2.0/Models/Consulta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoClinicaV2._0.Models
+{
+    public class Consulta
+    {
+        [Key]
+        public int IDConsulta { get; set; }
+        public string PlanoDeSaude { get; set; }
+        public DateTime DataConsulta { get; set; }
+    }
+}
