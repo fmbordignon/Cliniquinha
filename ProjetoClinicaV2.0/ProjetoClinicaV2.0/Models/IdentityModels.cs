4912f3a4e63e7ac4802fc91c62ffc55eaf081615
Felipe <felipe.bordignon@acad.pucrs.br>
2017-08-22 14:18:13 -0300

    cara eu só quero dar pull vsf

---
+++ ProjetoClinicaV2.0/ProjetoClinicaV2.0/Models/IdentityModels.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ProjetoClinicaV2._0.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual Medico Medico { get; set; }
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Observe que o authenticationType deve corresponder àquele definido em CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Adicionar declarações de usuário personalizado aqui
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+        public DbSet<Medico> Medico { get; set; }
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ ProjetoClinicaV2.0/ProjetoClinicaV2.0/Models/Paciente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoClinicaV2._0.Models
+{
+    public class Paciente
+    {
+        [Key]
+        public int IDPaciente { get; set; }
+        public string Nome { get; set; }
+        public long Telefone { get; set; }
+        public string Endereco { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public List<Consulta> ListaConsultas { get; set; }
+    }
+}
