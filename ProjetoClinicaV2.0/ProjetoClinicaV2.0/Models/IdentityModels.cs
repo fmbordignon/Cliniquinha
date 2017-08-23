adc5bca7b25c763e16de7e4ecc2241dbcd1f372f
Vinicius Cerutti <vinicius.cerutti@acad.pucrs.br>
2017-08-23 15:37:38 -0300

    (IdentityModels.cs) Adicionados os campos do usuário

--- ProjetoClinicaV2.0/ProjetoClinicaV2.0/Models/IdentityModels.cs
+++ ProjetoClinicaV2.0/ProjetoClinicaV2.0/Models/IdentityModels.cs
@@ -3,14 +3,22 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjetoClinicaV2._0.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public long Rg { get; set; }
+        [Required]
+        public long Telefone { get; set; }
+        [Required]
+        public string Endereco { get; set; }
         public virtual Medico Medico { get; set; }
-        public virtual Paciente Paciente { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Observe que o authenticationType deve corresponder àquele definido em CookieAuthenticationOptions.AuthenticationType
@@ -32,7 +40,5 @@ namespace ProjetoClinicaV2._0.Models
         }
         public DbSet<Medico> Medico { get; set; }
         public DbSet<Paciente> Paciente { get; set; }
-        public DbSet<Consulta> Consultas { get; set; }
-
     }
 }
