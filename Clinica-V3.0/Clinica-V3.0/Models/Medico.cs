4185e9df2b1eb4c295d31359194c6fbe1dcada8c
Vinicius Cerutti <vinicius.cerutti@acad.pucrs.br>
2017-08-24 17:08:14 -0300

    Adicionado projeto V3

---
+++ Clinica-V3.0/Clinica-V3.0/Models/Medico.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Clinica_V3._0.Models
+{
+    public class Medico
+    {
+        [Key]
+        public int IDMedico { get; set; }
+        public string Nome { get; set; }
+    }
+}
