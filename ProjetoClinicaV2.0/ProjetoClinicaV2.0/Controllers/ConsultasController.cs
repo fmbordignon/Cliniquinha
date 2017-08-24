a73e8722c5c25af23e35bfc49abb9ac3e091b824
Vinicius Cerutti <vinicius.cerutti@acad.pucrs.br>
2017-08-24 15:04:48 -0300

    Adicionado um novo projeto com cadastro de consulta

---
+++ ProjetoClinicaV2.0/ProjetoClinicaV2.0/Controllers/ConsultasController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjetoClinicaV2._0.Models;
+
+namespace ProjetoClinicaV2._0.Controllers
+{
+    public class ConsultasController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Consultas
+        public ActionResult Index()
+        {
+            return View(db.Consultas.ToList());
+        }
+
+        // GET: Consultas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consulta consulta = db.Consultas.Find(id);
+            if (consulta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consulta);
+        }
+
+        // GET: Consultas/Create
+        public ActionResult Create()
+        {
+            ViewBag.Pacientes = new SelectList(db.Paciente.Select(u => u.Nome).ToList(), "Name", "Name");
+            return View();
+        }
+
+        // POST: Consultas/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDConsulta,PlanoDeSaude,DataConsulta")] Consulta consulta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Consultas.Add(consulta);
+                db.SaveChanges();
+                ViewBag.Pacientes = new SelectList(db.Paciente.Select(u => u.Nome).ToList(), "Name", "Name");
+                //ViewBag.Medicos = new SelectList(db.Users.Where(u => u.Medico != null).Select(u => u.Nome).ToList(), "Name", "Name");
+                return RedirectToAction("Index");
+            }
+
+            return View(consulta);
+        }
+
+        // GET: Consultas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consulta consulta = db.Consultas.Find(id);
+            if (consulta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consulta);
+        }
+
+        // POST: Consultas/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDConsulta,PlanoDeSaude,DataConsulta")] Consulta consulta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(consulta).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(consulta);
+        }
+
+        // GET: Consultas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consulta consulta = db.Consultas.Find(id);
+            if (consulta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consulta);
+        }
+
+        // POST: Consultas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Consulta consulta = db.Consultas.Find(id);
+            db.Consultas.Remove(consulta);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProjetoClinicaV2.0/ProjetoClinicaV2.0/Models/Consulta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace ProjetoClinicaV2._0.Models
+{
+    public class Consulta
+    {
+        [Key]
+        public int IDConsulta { get; set; }
+        [Display(Name = "Plano de saude")]
+        public string PlanoDeSaude { get; set; }
+        [Display(Name = "Data da Consulta")]
+        [DataType(DataType.Date)]
+        public DateTime DataConsulta { get; set; }
+        [DisplayName("Paciente")]
+        public int IDPaciente { get; set; }
+        public virtual Paciente Paciente { get; set; }
+        public int IDMedico { get; set; }
+        public virtual Medico Medico { get; set; }
+
+    }
+}
